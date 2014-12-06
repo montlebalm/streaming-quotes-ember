c834dcafb6fec12d2acb0f9511235b9a5ce1455b
Chris Montrois <cmontrois@quickleft.com>
2014-12-06 12:47:06 -0700

    Simplifying sorting

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return {
-      watchlists: this.store.find('watchlist')
-    };
+    return this.store.find('watchlist');
   },
   setupController: function(controller, model) {
-    controller.set('watchlists', model.watchlists);
+    controller.set('watchlists', model);
   }
 });
