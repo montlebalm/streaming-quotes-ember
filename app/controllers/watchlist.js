7f7b23918a9949a025bebad92bfdcaf052719352
Chris Montrois <cmontrois@quickleft.com>
2014-12-06 22:32:47 -0700

    Minor cleanup

--- app/controllers/watchlist.js
+++ app/controllers/watchlist.js
@@ -1,4 +1,3 @@
-import _ from '../utils/underscore';
 import Ember from 'ember';
 import moment from '../utils/moment';
 
@@ -9,14 +8,15 @@ var LAST_UPDATED_FORMAT = 'h:mm:ss a';
 
 function stockToDisplay(stock, update) {
   var display = {
+    id: stock.get('id'),
     name: stock.get('name'),
     symbol: stock.get('symbol')
   };
 
-  _.each(COLUMNS, function(key) {
-    display[key] = {
-      value: stock.get(key),
-      last: (update && key in update) ? update[key][0] : stock.get(key)
+  COLUMNS.forEach(function(column) {
+    display[column] = {
+      value: stock.get(column),
+      last: (update && column in update) ? update[column][0] : stock.get(column)
     };
   });
 
@@ -38,10 +38,12 @@ export default Ember.Controller.extend({
   stocks: function() {
     var updates = this.get('updates');
 
-    return _.map(this.get('watchlist.stocks').toArray(), function(stock) {
+    console.log('renegerate stocks');
+
+    return this.get('watchlist.stocks').map(function(stock) {
       return stockToDisplay(stock, updates[stock.get('symbol')]);
     });
-  }.property('updates'),
+  }.property('watchlist.stocks.[]', 'updates'),
   updates: {},
-  watchlist: {}
+  watchlist: { stocks: [] }
 });
